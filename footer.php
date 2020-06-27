262e07364f5a59c3c14a675b6862f901d2d8ccf6
Janis Ozolins <todlebensjanis@gmail.com>
2020-06-28 02:20:10 +0300

    Changed_header_and_footer

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
 <footer class="main-footer">This is the footer</footer>
+
+< ?php wp_footer(); ?>
+
+© <?php the_time('Y'); ?> <?php bloginfo('name'); ?><br />
+<a href="<?php bloginfo('rss2_url'); ?>">Grab the feed</a>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,6 +3,17 @@
 get_header();
 
 echo("Hello world!");
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+<?php the_content(); ?>
+
+<?php endwhile; else: ?>
+
+ <h2>Woops...</h2>
+     
+ <p>Sorry, no posts we're found.</p>
+     
+<?php endif; ?>
 
 get_footer();
 
