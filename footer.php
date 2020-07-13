123007701dc4434abc6b4cf85c3a1c838add4d73
Jānis Ozoliņš <41022156+janisozo@users.noreply.github.com>
2020-07-13 09:17:35 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">This is the footer</footer>
-
-< ?php wp_footer(); ?>
-
-© <?php the_time('Y'); ?> <?php bloginfo('name'); ?><br />
-<a href="<?php bloginfo('rss2_url'); ?>">Grab the feed</a>
-
-</body>
-</html>
